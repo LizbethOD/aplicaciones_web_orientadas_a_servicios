5ff7855119c42687e4f65eeec0921328967129df
LIZBETH ORTEGA <>
2021-04-24 02:45:18 +0000

    insertar y leer

--- api_insertar_leer/insertaryleer.py
+++ api_insertar_leer/insertaryleer.py
@@ -1,7 +1,7 @@
 import web
 import json
 urls = (
-    '/agenda_datos?', 'Agenda',)    
+    '/agenda?', 'Agenda',)    
 app = web.application(urls, globals())
 
 class Agenda():
@@ -15,34 +15,34 @@ class Agenda():
 
     if action=="put":
       try:
-        nombre_usuario = parametros.nombre
-        fecha_nacimiento= parametros.fecha
-        año=fecha_nacimiento[6:10]
+        nombre = parametros.nombre
+        fecha = parametros.fecha
+        año=fecha[6:10]
         edad=2021-int(año)
         
         persona = {
-        "nombre": nombre_usuario,
+        "nombre": nombre,
         "edad": edad
         }
-        with open("datos.json") as file:
+        with open("agenda.json") as file:
           self.archivo_json = json.load(file) 
         self.archivo_json["agenda"].append(persona)
         print(self.archivo_json)  
-        with open("datos.json","w") as file:
+        with open("agenda.json","w") as file:
           json.dump(self.archivo_json, file)
         return(persona)
         
       except:
-        return "Los datos no se ingresaron de la forma correcta"
+        return "Los datos no fueron ingresados correctamente..."
 
   def getAction(self):
     try:
-      with open("datos.json") as file:
-        self.datos_json = json.load(file)
-      return json.dumps(self.datos_json)     
+      with open("agenda.json") as file:
+        self.agenda_json = json.load(file)
+      return json.dumps(self.agenda_json)     
     except Exception as error:
       print("Error {}".format(error.args[0]))
 
     	
-  if _name_ == "_main_":
+  if __name__ == "__main__":
     app.run()
